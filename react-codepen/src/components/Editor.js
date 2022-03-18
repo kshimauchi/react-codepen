16b0ece5f1bba7704c53a21ae13a6f99def91d3e
unknown <kshimauchi@gmail.com>
2022-03-17 18:05:01 -0800

    initial commit

---
+++ react-codepen/src/components/Editor.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import {Conroller as ControlledEditior} from 'react-codemirror2'
+
+
+export default function Editor() {
+  return (
+    <div>
+        Editor
+    </div>
+  )
+}
